a6b16dfd6b0f6bec8b64a0b6ed8157d1504df21b
Saidi <saidi.ahamada@gmail.com>
2023-08-11 09:05:14 +0200

    update census script

--- census/main.py
+++ census/main.py
@@ -1,14 +1,14 @@
 from openpyxl import load_workbook
 
-workbook = load_workbook(filename='censuspopdata.xlsx')
+workbook = load_workbook(filename='census/census.xlsx')
 
-sheet_ranges = workbook.active
+sheet_object = workbook.active
 result_population = {}
-current_county = sheet_ranges.cell(row=2, column=3).value
+current_county = sheet_object.cell(row=2, column=3).value
 total_population = 0
-for i in range(2, 15):
-    county = sheet_ranges.cell(row=i, column=3).value
-    nb_population_county = sheet_ranges.cell(row=i, column=4).value
+for i in range(2, sheet_object.max_row):
+    county = sheet_object.cell(row=i, column=3).value
+    nb_population_county = sheet_object.cell(row=i, column=4).value
     if current_county == county:
         total_population += nb_population_county
     else:
